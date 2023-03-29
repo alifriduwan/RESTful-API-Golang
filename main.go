aa65c8ada475b29307e8b847afdd1e77d4aac09f
alif riduwan <alifriduwan.a@gmail.com>
2023-03-29 22:37:46 +0700

    ทำการสร้าง webserver and Routing

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello Server!")
+	r := gin.Default() //Create web server
+
+	// http://example.com/hello GET POST PATCH DELETE
+	// GET -> ส่งข้อมูลออกมาในรูปแบบ json / ส่ง status code
+	//json {"key": "value"} => {"greeting":"Hello Server"}
+	r.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"greeting": "Hello Server"})
+	})
+
+	r.Run()
 }
